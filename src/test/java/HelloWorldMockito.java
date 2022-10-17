d3e8a0104b1f134ea79e0e5561e045e64b5caef6
Jonas César Silva <jonascesar.silva@dxc.com>
2022-10-17 12:00:57 -0300

    Primeiro teste utilizando mockito

---
+++ src/test/java/HelloWorldMockito.java
@@ -0,0 +1,22 @@
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import br.com.alura.leilao.dao.LeilaoDao;
+import br.com.alura.leilao.model.Leilao;
+import junit.framework.Assert;
+
+public class HelloWorldMockito {
+	
+
+	@Test
+	void hello() {
+		LeilaoDao mock = Mockito.mock(LeilaoDao.class);		
+		List<Leilao> todos = mock.buscarTodos();
+		assertTrue(todos.isEmpty());
+		
+	}
+}
